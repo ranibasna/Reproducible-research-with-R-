64fee141d09a06690e61aa19366e3059a9a45f67
Rani Basna <ranibasna@yahoo.com>
2022-11-24 09:43:14 +0100

    changing the number of clusters to 4

--- clustering_code/cluster_code.R
+++ clustering_code/cluster_code.R
@@ -7,7 +7,7 @@ df <- scale(USArrests)
 set.seed(123)
 # we will now apply kmeans method to cluster the data
 #  
-km.res <- kmeans(df, 5, nstart = 25)
+km.res <- kmeans(df, 4, nstart = 25)
 clustering_result <- cbind(USArrests, cluster = km.res$cluster)
 write.csv(clustering_result, file = args[2])
 
